ef872ac4c7d6d3d34881dcab06a76264dcf100ac
liuzihan123 <2234237217@qq.com>
2019-05-06 18:23:19 +0800

    完善薪资添加，判断工号是否存在

--- api/v1/auth.js
+++ api/v1/auth.js
@@ -556,14 +556,24 @@ async function SalaryQuery(req, queryInfo, res) {
 //薪资信息添加
 router.post('/salary/add', async (req, res) => {
   try {
-    // let obj = { ...req.body };
-    var salary = new Salary(req.body)
-    await salary.save();
-    res.json({
-      code: 1,
-      status: "success",
-      info: "添加成功",
-    })
+    console.log(req.body)
+    const count = await Salary.countDocuments({ tid: req.body.tid })
+    console.log("111" + count)
+    if (count) {
+      res.json({
+        code: 2,
+        status: 'error',
+        info: "该教师工号已经存在"
+      })
+    } else {
+      var salary = new Salary(req.body)
+      await salary.save();
+      res.json({
+        code: 1,
+        status: "success",
+        info: "添加成功",
+      })
+    }
   } catch (error) {
     res.json({
       code: 0,
